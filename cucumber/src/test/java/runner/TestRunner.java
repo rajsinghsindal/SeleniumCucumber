b1e0a4c4cadbee9bdd294f1ff3333a4310dd1e08
Shubham Gupta <shubhagg@192.168.1.2>
2020-04-17 13:28:03 +0530

    WEBDriver Manager

--- cucumber/src/test/java/runner/TestRunner.java
+++ cucumber/src/test/java/runner/TestRunner.java
@@ -5,9 +5,10 @@ import org.testng.annotations.Test;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @Test
 //@RunWith(Cucumber.class)
-@CucumberOptions(features ="Feature",glue="stepDefination",plugin={"pretty", "html:target/cucumber-reports" ,"json:target/cucumber.json"})
+@CucumberOptions(features ="Feature",glue="stepDefination",plugin={"pretty", "html:target/cucumber-reports","json:target/cucumber.json"})
  
 public class TestRunner extends AbstractTestNGCucumberTests {
 
